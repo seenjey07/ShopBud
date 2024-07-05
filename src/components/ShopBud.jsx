54a57beaf8aca1baf5b5ad4b05f683227ebaf032
Christine Rosales <138593141+seenjey07@users.noreply.github.com>
2024-07-05 16:20:28 +0800

    Modified button styles, prepared the Edit button, and installed the uuid package.

--- src/components/ShopBud.jsx
+++ src/components/ShopBud.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { v4 as uuid } from "uuid";
 
 const ShopBud = () => {
   const [item, setItem] = useState("");
@@ -6,29 +7,38 @@ const ShopBud = () => {
 
   return (
     <>
-      <div className="flex flex-col align-center w-full max-w-2xl p-4 bg-white rounded-lg shadow-md">
-        <h1 className="text-2xl font-bold mb-5 underline text-center text-blue-800">
-          ShopBud
+      <div className="flex flex-col align-center w-full max-w-2xl p-4 bg-blue-100 rounded-lg shadow-md">
+        <h1 className="text-2xl font-bold mb-5 text-center text-blue-800">
+          🛒ShopBud
         </h1>
-        <div className="flex flex-col justify-between w-full mb-4">
-          <div className="flex justify-between">
+        <div className="flex flex-col justify-between w-full">
+          <div className="flex justify-between mb-4">
             <input
-              className="border-1 flex-grow p-2 rounded"
+              className="border-1 flex-grow p-2 text-sm rounded"
               type="text"
-              placeholder="Enter item"
+              placeholder="Enter item..."
               value={item}
               onChange={(e) => setItem(e.target.value)}
             />
 
             <button
-              className="ml-4 py-1.5 px-3.5 text-sm font-semibold text-black bg-yellow-400 border-none rounded-md cursor-pointer"
-              onClick={() => setListItems([...listItems, item])}
+              className="ml-4 py-1.5 px-3.5 text-xs font-semibold text-black bg-yellow-400 border-none rounded-md cursor-pointer"
+              onClick={() =>
+                setListItems([...listItems, { id: uuid(), itemName: item }])
+              }
             >
               Add
             </button>
 
             <button
-              className="ml-2 py-1 px-2 text-sm font-semibold text-black bg-red-500 border-none rounded-md cursor-pointer"
+              className="ml-2 py-1.5 px-3.5 text-xs font-semibold text-black bg-orange-400 border-none rounded-md cursor-pointer"
+              onClick={() => setItem()}
+            >
+              Edit
+            </button>
+
+            <button
+              className="ml-2 py-1 px-2 text-xs font-semibold text-black bg-red-500 border-none rounded-md cursor-pointer"
               onClick={() => {
                 setListItems(listItems.filter((i) => i !== item));
                 setItem("");
